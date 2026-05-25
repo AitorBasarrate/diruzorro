40740c046828c5c44138134570f01626384f2896
AitorBasarrate <aitor.basarrateastobiza@plexus.es>
2026-05-25 13:28:39 +0200

    Month balance graph

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:diruzorro/views/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('es_ES');
   runApp(
     const ProviderScope(
       child: DiruzorroApp(),
